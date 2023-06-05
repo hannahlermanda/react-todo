ffdf9d4f637e38925a6dbc2c52754d49a4f39365
hannahlermanda <hannahlermanda@gmail.com>
2023-06-04 18:44:48 -0700

    Week 1 lesson, unordered list and heading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
+const toDoList = [
+  {id: 1, title: "Wake up"},
+  {id: 2, title: "Drink water"},
+  {id: 3, title: "Brush teeth"}
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <div style={{ textAlign: 'center', backgroundColor: "blue" }}>
+          <h1> To-Do List </h1>
+        </div>
+        <div style={{ textAlign: 'left', backgroundColor: "aquamarine" }}>
+          <ul>
+            {toDoList.map(function (todo) {
+              return <li key={todo.id}>{todo.title}</li>;
+            })}
+          </ul>
+          </div>
       </header>
-    </div>
   );
 }
 
